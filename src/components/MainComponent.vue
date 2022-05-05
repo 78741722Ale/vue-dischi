4fc3f7627566bf03ed17a5ad9f891fc843d2f798
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-05 17:02:57 +0200

    Creato singola card per iniettare in modo dinamico

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,17 +1,19 @@
 <template>
     <!-- Genitore -->
     <div class="container-fluid flex_cent h_main bg_main">
-        <div class="row w-100 flex_cent gap-4 h-75">
-            <div class="col-2 h-50 bg-primary">A</div>
-            <div class="col-2 h-50 bg-primary">A</div>
-            <div class="col-2 h-50 bg-primary">A</div>
-            <div class="col-2 h-50 bg-primary">A</div>
-            <div class="col-2 h-50 bg-primary">A</div>
-            <div class="col-2 h-50 bg-primary">A</div>
-            <div class="col-2 h-50 bg-primary">A</div>
-            <div class="col-2 h-50 bg-primary">A</div>
-            <div class="col-2 h-50 bg-primary">A</div>
-            <div class="col-2 h-50 bg-primary">A</div>
+        <div class="row w-100 flex_cent gap-4 h-100">
+            <!-- Verificato in caso di V-for va bene -->
+            <!-- Questa è una singola card -->
+            <div class="col-2 p-0 h_card bg-primary flex_col">
+                <!-- Immagine -->
+                <div class="col-12 h_xl bg-warning w-100">
+                    <img src="https://picsum.photos/300/300" alt="Andrà Titolo Dinamico">
+                </div>
+                <!-- Titolo -->
+                <div class="col-12 h_sm bg-danger w-100">B</div>
+                <!-- Descrizione Brano -->
+                <div class="col-12 h_sm bg-danger w-100">B</div>
+            </div>
         </div>
     </div>
 </template>
@@ -39,6 +41,36 @@ export default {
     align-items: center;
 }
 
+.flex_col {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.col-12 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+        width: 90%;
+        height: 90%;
+        background-position: center center;
+        padding: 0;
+    }
+}
+
+// Altezze della card
+
+.h_card { 
+    height: 425px;
+}
+.h_xl {
+    height: 60%;
+}
+.h_sm {
+    height: 20%;
+}
 
 
 </style>
