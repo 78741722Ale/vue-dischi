15071b52ec48cb3066566478d5945bc39977034c
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-05 16:41:17 +0200

    Creato singole card all'interno del main

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,8 +1,17 @@
 <template>
     <!-- Genitore -->
-    <div class="container-fluid h_main bg_main">
-        <div class="row">
-            <div class="col-5"></div>
+    <div class="container-fluid flex_cent h_main bg_main">
+        <div class="row w-100 flex_cent gap-4 h-75">
+            <div class="col-2 h-50 bg-primary">A</div>
+            <div class="col-2 h-50 bg-primary">A</div>
+            <div class="col-2 h-50 bg-primary">A</div>
+            <div class="col-2 h-50 bg-primary">A</div>
+            <div class="col-2 h-50 bg-primary">A</div>
+            <div class="col-2 h-50 bg-primary">A</div>
+            <div class="col-2 h-50 bg-primary">A</div>
+            <div class="col-2 h-50 bg-primary">A</div>
+            <div class="col-2 h-50 bg-primary">A</div>
+            <div class="col-2 h-50 bg-primary">A</div>
         </div>
     </div>
 </template>
@@ -19,11 +28,17 @@ export default {
     height: calc(100% - 7vh);
     width: 100%;
 }
-
 // Colore di Background
 .bg_main {
     background-color: #1E2D3B;
 }
+// Utilities
+.flex_cent {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
 
 
 </style>
