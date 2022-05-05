d3c93545a829855910b7ed66db72b066ffcc2b54
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-05 16:31:23 +0200

    Header completato con tanto di Logo

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container-fluid bg_header h_header">
-        <div class="row">
-            <div class="col-3">
-                A
+        <div class="row h-100">
+            <div class="col-3 d-flex justify-content-start align-items-center">
+                <img src="@/assets/logo.png" alt="Logo Spotify">
             </div>
         </div>
     </div>
@@ -25,7 +25,13 @@ export default {
     background-color: #2E3A46;
 }
 
-
+.col-3 {
+    img {
+        width: 60px;
+        height: 60px;
+        aspect-ratio: 1 / 1;
+    }
+}
 
 </style>
 
