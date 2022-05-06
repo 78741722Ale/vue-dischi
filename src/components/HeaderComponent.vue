50956b919d3e84fb5f2bafd9596e16e389f147ae
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-06 14:31:00 +0200

    Aggiunto menù dropdown (a tendina) per la scelta del genere musicale

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,8 +1,30 @@
 <template>
     <div class="container-fluid bg_header h_header">
-        <div class="row h-100">
+        <div class="row justify-content-between h-100">
+            <!-- Logo Spotify -->
             <div class="col-3 d-flex justify-content-start align-items-center">
                 <img src="@/assets/logo.png" alt="Logo Spotify">
+            </div>
+            <!-- Input type search -->
+            <div class="col-3 d-flex justify-content-center align-items-center">
+                <li class="nav-item dropdown flex_cent">
+                    <!-- Toggle del dropdown -->
+                    <a class="nav-link dropdown-toggle" 
+                    href="#" 
+                    id="navbarDropdown" 
+                    role="button" 
+                    data-bs-toggle="dropdown" 
+                    aria-expanded="false">
+                        Genere
+                    </a>
+                    <!-- Menu dropdown -->
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Rock</a></li>
+                        <li><a class="dropdown-item" href="#">Pop</a></li>
+                        <li><a class="dropdown-item" href="#">Jazz</a></li>
+                        <li><a class="dropdown-item" href="#">Metal</a></li>
+                    </ul>
+                </li>
             </div>
         </div>
     </div>
